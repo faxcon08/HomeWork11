eeb64ad02c46f1060386dcf8f9acd9dde2343c8e
faxcon08 <faxcon-08@mail.ru>
2022-12-10 17:21:08 +0300

    First & Second Task

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,48 @@
+
+
+
 public class Main {
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_RED = "\u001B[31m";
-    public static void main(String[] args) {
+    static int TASK_NUMBER=1;
+    static void printTaskNumber(String number){
+        System.out.println(ANSI_RED + "1."+(TASK_NUMBER++)+" "+number+" Task" + ANSI_RESET);
+    } // printTasKMumber
 
-        System.out.println("Hello world!");
+    static void checkLeapYear(int year) {
+        if (year < 0) {
+            throw new RuntimeException("Отрицательный год");
+        }
+        if((year%4==0 || year%100==0) && year%400!=0) {
+            System.out.println(year + " - високосный год");
+            return;
+        }
+        System.out.println(year + " - не високосный год");
+
+    }// checkLeapYer
+    static void printMessageForVersion (int iOS, int clientDeviceYear) {
+        if((iOS!=0 && iOS!=1) || clientDeviceYear<1990)
+            throw new RuntimeException("Некорректный год или версия OS");
+
+        String iOSVersion = (iOS == 0) ? "Android" : "iOS";
+        if (clientDeviceYear<2015) {
+            System.out.println("Установите облегченную версию приложения для "+iOSVersion+" по ссылке");
+        } else
+            System.out.println("Установите  приложения для "+iOSVersion+" по ссылке");
+
     }
-}
+
+    public static void main(String[] args) {
+        printTaskNumber("First"); ///////////////// Fist /////////////////
+        int year = java.time.LocalDate.now().getYear();
+        checkLeapYear(year);
+
+        printTaskNumber("Second"); ////////////// Second ////////////////
+        int iOS = 1;
+        int clientDeviceYear = 2013;
+        printMessageForVersion(iOS,clientDeviceYear);
+
+    }// main
+
+
+}// Main
